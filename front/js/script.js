39828692288d2c4c0fa6be24823b01084f2c86bd
Alexandre Audigié <alexandre.audigie@orange.fr>
2022-03-21 20:21:51 +0100

    products affichés sur page index

---
+++ front/js/script.js
@@ -0,0 +1,22 @@
+fetch("http://localhost:3000/api/products")
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function(value) {
+    console.log(value);
+    for (let product of value){
+      document.getElementById("items").innerHTML += `<a href="./product.html?id=42">
+                                                        <article>
+                                                          <img src="${product.imageUrl}" alt="${product.altTxt}">
+                                                          <h3 class="productName">${product.name}</h3>
+                                                          <p class="productDescription">${product.description}</p>
+                                                        </article>
+                                                      </a>`
+    }
+  })
+  .catch(function(err) {
+    document.getElementById("items").innerHTML = "<p>Items non chargés, veuillez vérifier la connexion du serveur de l'api</p>"
+    // Une erreur est survenue
+});
