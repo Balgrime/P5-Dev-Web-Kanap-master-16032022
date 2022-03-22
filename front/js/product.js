e3fd63d205cc8ddac6023f86bfa5c62a276af829
Alexandre Audigié <alexandre.audigie@orange.fr>
2022-03-22 20:56:25 +0100

    infos page product reliées

--- front/js/product.js
+++ front/js/product.js
@@ -4,3 +4,32 @@ console.log(urlPageProduct);
 let urlSearchParams = new URLSearchParams(urlPageProduct);
 let idProductActif = urlSearchParams.get("id");
 console.log(idProductActif);
+
+
+
+fetch("http://localhost:3000/api/products")
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function(value) {
+    console.log(value);
+    let objectActif = value.find(element => element._id === idProductActif);
+    console.log(objectActif);
+    document.getElementById("title").innerHTML = `${objectActif.name}`;
+    let imgCanapé = document.getElementsByClassName("item__img");
+    imgCanapé[0].innerHTML = `<img src="${objectActif.imageUrl}" alt="${objectActif.altTxt}">`;
+
+    document.getElementById("price").innerHTML = `${objectActif.price}`;
+    document.getElementById("description").innerHTML = `${objectActif.description}`;
+
+    for (let chosenColor of objectActif.colors){
+        document.getElementById("colors").innerHTML += `<option value="${chosenColor}">${chosenColor}</option>`
+        console.log(chosenColor);
+    }
+  })
+  .catch(function(err) {
+    document.getElementById("title").innerHTML = "<p>Item non chargé, veuillez vérifier la connexion du serveur de l'api</p>"
+    // Une erreur est survenue
+});
