f7050df74772c0354b37d6b71f6d0008fd3056cf
Alexandre Audigié <alexandre.audigie@orange.fr>
2022-04-22 21:55:31 +0200

    quantité dynamique optimisée dans page panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -24,7 +24,7 @@ for (let product of arrayCart){
 
     function showValue(value){
         console.log(value.price);
-           document.getElementById("cart__items").innerHTML += `<article class="cart__item" data-id="${value.id}" data-color="${product.color}">
+           document.getElementById("cart__items").innerHTML += `<article class="cart__item" data-id="${product.id}" data-color="${product.color}">
                                                                     <div class="cart__item__img">
                                                                         <img src="${value.imageUrl}" alt="Photographie d'un canapé">
                                                                     </div>
@@ -37,18 +37,16 @@ for (let product of arrayCart){
                                                                         <div class="cart__item__content__settings">
                                                                         <div class="cart__item__content__settings__quantity">
                                                                             <p>Qté : </p>
-                                                                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.value}">
+                                                                            <input data-key= '${product.id}${product.color}' data-id= '${product.id}' data-color= '${product.color}' type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.value}">
                                                                         </div>
                                                                         <div class="cart__item__content__settings__delete">
-                                                                            <p class="deleteItem">Supprimer</p>
+                                                                            <p data-key= '${product.id}${product.color}' data-id= '${product.id}' data-color= '${product.color}' class="deleteItem">Supprimer</p>
                                                                         </div>
                                                                         </div>
                                                                     </div>
                                                                     </article>`
     };
 };
-
-
 
 
 
@@ -93,24 +91,58 @@ displayNumberArticleAndTotal();
 };*/
 
 
-addEventListener("change", function(){
-    
-    for(let a=0; a<arrayCart.length;a++){
+let idCartItems = document.getElementById("cart__items");
+idCartItems.addEventListener("change", changeQuantityOfItem);
+
+
+
+
+
+
+
+   /*for(let a=0; a<arrayCart.length;a++){
         console.log("une valeur a changée!");
         let classQuantity = document.getElementsByClassName("itemQuantity")[a];
         let activQuantity = parseInt(classQuantity.value);
         console.log(activQuantity);
         changeQuantityOfItemInArrayCart(arrayCart[a].id, arrayCart[a].color, activQuantity, a);
     };
+    };
     console.log(arrayCart);
     Storage.save("arrayCart", arrayCart);
-});
+});*/
 
 
 
-function changeQuantityOfItemInArrayCart(id, color, chosenQuantity, position){
-      let newProduct = new Cart (id + color, chosenQuantity, id, color);
-      arrayCart.splice(position, 1, newProduct);
+function changeQuantityOfItem(e){
+        console.log("une value a changée");
+    let cartKey = e.target.dataset.key;
+    console.log(cartKey);
+    let cartQuantity = parseInt(e.target.value);
+    console.log(cartQuantity);
+    let cartId = e.target.dataset.id;
+    console.log(cartId);
+    let cartColor = e.target.dataset.color;
+    console.log(cartColor);
+
+
+
+    let positionOfProductInArray = 0;
+
+    for (let a = 0; a<arrayCart.length; a++){
+        if (arrayCart[a].key === cartKey){
+          positionOfProductInArray = a;
+          console.log(positionOfProductInArray);
+        };
+    };
+
+
+
+    let newProduct = new Cart (cartKey, cartQuantity, cartId, cartColor);
+    arrayCart.splice(positionOfProductInArray, 1, newProduct);
+    
+
+    Storage.save("arrayCart", arrayCart);
   };
 
 
