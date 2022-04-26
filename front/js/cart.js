93f7742fe719773350a81d1fc6729b74c76b3a2d
Alexandre Audigié <alexandre.audigie@orange.fr>
2022-04-26 14:46:23 +0200

    ajout confirmation et contact

--- front/js/cart.js
+++ front/js/cart.js
@@ -22,46 +22,50 @@ console.log(arrayCart);
 
 
 for (let product of arrayCart){
-    loadProductActif(product.id).then(showValue).catch(displayError);
+    loadProductActif(product.id).then(function(value){
+        showValue(value, product);
+
+    }).catch(displayError);
     console.log(product.id);
 
-    function showValue(value){
-        console.log(value.price);
-        total += value.price * product.value;
-        console.log(total);
-        numberItems += product.value;
-           document.getElementById("cart__items").innerHTML += `<article class="cart__item" data-id="${product.id}" data-color="${product.color}">
-                                                                    <div class="cart__item__img">
-                                                                        <img src="${value.imageUrl}" alt="Photographie d'un canapé">
-                                                                    </div>
-                                                                    <div class="cart__item__content">
-                                                                        <div class="cart__item__content__description">
-                                                                        <h2>${value.name}</h2>
-                                                                        <p>${product.color}</p>
-                                                                        <p id='price${product.id}${product.color}' >${value.price * product.value} €</p>
-                                                                        </div>
-                                                                        <div class="cart__item__content__settings">
-                                                                        <div class="cart__item__content__settings__quantity">
-                                                                            <p>Qté : </p>
-                                                                            <input data-key= '${product.id}${product.color}' data-id= '${product.id}' data-color= '${product.color}' type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.value}">
-                                                                        </div>
-                                                                        <div class="cart__item__content__settings__delete">
-                                                                            <p data-key= '${product.id}${product.color}' data-id= '${product.id}' data-color= '${product.color}' class="deleteItem">Supprimer</p>
-                                                                        </div>
-                                                                        </div>
-                                                                    </div>
-                                                                    </article>`
+    
+};
 
-            document.getElementById("totalPrice").innerHTML = total;
-            document.getElementById("totalQuantity").innerHTML = numberItems;
-    };
+function showValue(value, product){
+    console.log(value.price);
+    total += value.price * product.value;
+    console.log(total);
+    numberItems += product.value;
+       document.getElementById("cart__items").innerHTML += `<article class="cart__item" data-id="${product.id}" data-color="${product.color}">
+                                                                <div class="cart__item__img">
+                                                                    <img src="${value.imageUrl}" alt="Photographie d'un canapé">
+                                                                </div>
+                                                                <div class="cart__item__content">
+                                                                    <div class="cart__item__content__description">
+                                                                    <h2>${value.name}</h2>
+                                                                    <p>${product.color}</p>
+                                                                    <p id='price${product.id}${product.color}' >${value.price * product.value} €</p>
+                                                                    </div>
+                                                                    <div class="cart__item__content__settings">
+                                                                    <div class="cart__item__content__settings__quantity">
+                                                                        <p>Qté : </p>
+                                                                        <input data-key= '${product.id}${product.color}' data-id= '${product.id}' data-color= '${product.color}' type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.value}">
+                                                                    </div>
+                                                                    <div class="cart__item__content__settings__delete">
+                                                                        <p data-key= '${product.id}${product.color}' data-id= '${product.id}' data-color= '${product.color}' class="deleteItem">Supprimer</p>
+                                                                    </div>
+                                                                    </div>
+                                                                </div>
+                                                                </article>`
+
+        document.getElementById("totalPrice").innerHTML = total;
+        document.getElementById("totalQuantity").innerHTML = numberItems;
 };
 
 
 
-
-
-function displayError(){
+function displayError(error){
+    console.log(error);
     document.getElementById("cart__items").innerHTML = "<p>Item non chargé, veuillez vérifier la connexion du serveur de l'api</p>"
   };
   
@@ -112,8 +116,6 @@ function changeQuantityOfItem(e){
     let cartColor = e.target.dataset.color;
     console.log(cartColor);
 
-
-    
 
     let newPrice = 0;
     let positionOfProductInArray = 0;
@@ -242,3 +244,51 @@ function removeOneProduct(e){
         document.getElementById("totalQuantity").innerHTML = "";
     };
 };
+
+
+
+
+
+
+
+
+
+
+import { Contact } from "./contact.js";
+
+let firstName = document.getElementById("firstName");
+let lastName = document.getElementById("lastName");
+let address = document.getElementById("address");
+let city = document.getElementById("city");
+let email = document.getElementById("email");
+let order = document.getElementById("order");
+
+let firstNameErrorMsg = document.getElementById("firstNameErrorMsg");
+let lastNameErrorMsg = document.getElementById("lastNameErrorMsg");
+let addressErrorMsg = document.getElementById("addressErrorMsg");
+let cityErrorMsg = document.getElementById("cityErrorMsg");
+let emailErrorMsg = document.getElementById("emailErrorMsg");
+let orderErrorMsg = document.getElementById("orderErrorMsg");
+
+
+
+order.addEventListener("submit", (e) =>{
+    let ErrorMsg = [];
+
+    if(firstName.value == "test"){
+        console.log("test concluant");
+        firstNameErrorMsg.innerHTML = "ceci est un test réussi";
+        ErrorMsg.push(firstNameErrorMsg);
+    }
+
+
+
+
+
+
+    if (ErrorMsg.length > 0){
+        e.preventDefault();
+    } else {
+        let contact = new Contact(firstName, lastName, address, city, email);
+    }
+});
